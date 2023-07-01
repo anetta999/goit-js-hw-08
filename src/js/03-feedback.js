ca60ee0d662240873980a4bfd7a74f6e6f827405
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-07-01 12:40:40 +0300

    completed 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+form.addEventListener('input', throttle(handlerFormInput, 500));
+form.addEventListener('submit', handlerFormSubmit);
+
+const LS_KEY = 'feedback-form-state';
+const data = {};
+function handlerFormInput(evt) {
+  data[evt.target.name] = evt.target.value;
+  //   console.log(data);
+
+  const LSData = localStorage.setItem(LS_KEY, JSON.stringify(data));
+  const savedData = localStorage.getItem(LS_KEY);
+  const parsedSavedData = JSON.parse(savedData);
+  if (LSData) {
+    email.value = parsedSavedData.email;
+    textarea.value = parsedSavedData.message;
+  }
+}
+
+function handlerFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(LS_KEY);
+
+  console.log(data);
+}
