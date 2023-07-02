a4c1f6d016e66de3ef2b4ebdec60b0cb10080aa1
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-07-02 11:24:47 +0300

    fix 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,20 +7,13 @@ form.addEventListener('input', throttle(handlerFormInput, 500));
 form.addEventListener('submit', handlerFormSubmit);
 
 const LS_KEY = 'feedback-form-state';
-const data = {};
+let data = JSON.parse(localStorage.getItem(LS_KEY)) || {};
+populateForm();
 function handlerFormInput(evt) {
   data[evt.target.name] = evt.target.value;
   //   console.log(data);
 
   localStorage.setItem(LS_KEY, JSON.stringify(data));
-
-  const savedData = JSON.parse(localStorage.getItem(LS_KEY));
-  console.log(savedData);
-
-  if (savedData) {
-    email.value = savedData.email;
-    textarea.value = savedData.message;
-  }
 }
 
 function handlerFormSubmit(evt) {
@@ -30,4 +23,15 @@ function handlerFormSubmit(evt) {
   localStorage.removeItem(LS_KEY);
 
   console.log(data);
+  data = {};
+}
+
+function populateForm() {
+  const savedData = JSON.parse(localStorage.getItem(LS_KEY));
+  console.log(savedData);
+
+  if (savedData) {
+    email.value = savedData.email || '';
+    textarea.value = savedData.message || '';
+  }
 }
